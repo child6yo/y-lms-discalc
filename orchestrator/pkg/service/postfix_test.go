d9d12e8fa218ff7f77258f8eff1b3e28716d4dac
Arseniy Glushkov <miguelprod8@gmail.com>
2025-04-30 18:26:30 +0700

    feat: service test

--- orchestrator/pkg/service/postfix_test.go
+++ orchestrator/pkg/service/postfix_test.go
@@ -1,50 +1,51 @@
 package service
 
-// import (
-// 	"slices"
-// 	"testing"
-// )
+import (
+	"slices"
+	"testing"
+)
 
-// func TestPostfixExpression(t *testing.T) {
-// 	testCases := []struct {
-// 		expression string
-// 		expect     []string
-// 		error      bool
-// 	}{
-// 		{
-// 			"2+2*2",
-// 			[]string{"2", "2", "2", "*", "+"},
-// 			false,
-// 		},
-// 		{
-// 			"(2+2)*2",
-// 			[]string{"2", "2", "+", "2", "*"},
-// 			false,
-// 		},
-// 		{
-// 			"2*0",
-// 			[]string{"2", "0", "*"},
-// 			false,
-// 		},
-// 		{
-// 			"2*(2+2",
-// 			[]string{},
-// 			true,
-// 		},
-// 	}
+func TestPostfixExpression(t *testing.T) {
+	testCases := []struct {
+		expression string
+		expect     []string
+		error      bool
+	}{
+		{
+			"2+2*2",
+			[]string{"2", "2", "2", "*", "+"},
+			false,
+		},
+		{
+			"(2+2)*2",
+			[]string{"2", "2", "+", "2", "*"},
+			false,
+		},
+		{
+			"2*0",
+			[]string{"2", "0", "*"},
+			false,
+		},
+		{
+			"2*(2+2",
+			[]string{},
+			true,
+		},
+	}
 
-// 	for i, tc := range testCases {
-// 		ex, err := PostfixExpression(tc.expression)
-// 		if err != nil {
-// 			if tc.error {
-// 				continue
-// 			} else {
-// 				t.Errorf("test %d failed: unexpected error (%s)", i+1, err)
-// 			}
-// 		}
+	for i, tc := range testCases {
+		s := MainService{nil, nil}
+		ex, err := s.PostfixExpression(tc.expression)
+		if err != nil {
+			if tc.error {
+				continue
+			} else {
+				t.Errorf("test %d failed: unexpected error (%s)", i+1, err)
+			}
+		}
 
-// 		if slices.Compare(ex, tc.expect) != 0 {
-// 			t.Errorf("test %d failed", i+1)
-// 		}
-// 	}
-// }
+		if slices.Compare(ex, tc.expect) != 0 {
+			t.Errorf("test %d failed", i+1)
+		}
+	}
+}
